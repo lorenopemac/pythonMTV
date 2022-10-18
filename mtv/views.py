2d121c1f7d930e90996b6f407708497723b0b66c
Lorenzo <lorenzo.macintyre@id90travel.com>
2022-10-17 21:52:15 -0300

    Fix template route

--- mtv/views.py
+++ mtv/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "mtv/saludar.html")
